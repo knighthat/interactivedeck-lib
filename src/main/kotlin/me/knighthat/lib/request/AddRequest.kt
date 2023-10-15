1e2ddd97be3321823555664992cb43e5ea009a18
KnightHat <knightchanelgaming@gmail.com>
2023-10-15 16:16:40 -0500

    compress payload on serialize & decompress on initialize

--- src/main/kotlin/me/knighthat/lib/request/AddRequest.kt
+++ src/main/kotlin/me/knighthat/lib/request/AddRequest.kt
@@ -2,19 +2,44 @@ package me.knighthat.lib.request
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import me.knighthat.lib.json.Json
 import java.util.*
 import java.util.function.Consumer
 
 class AddRequest(
-        payload: JsonElement,
-        uuid: UUID?,
-        target: Target
+    payload: JsonElement,
+    uuid: UUID?,
+    target: Target
 ) : TargetedRequest(RequestType.ADD, payload, uuid, target) {
 
-    constructor(uuid: UUID, payload: JsonArray) : this(payload, uuid, Target.BUTTON)
+    constructor(uuid: UUID, payload: JsonArray) : this(JsonArray(), uuid, Target.BUTTON) {
+        this.payload
+            .asJsonArray
+            .addAll(
+                if (Json.isGzip(payload)) {
+                    val array = payload.asJsonArray
+                    Json.gzipDecompress(array).asJsonArray
+                } else
+                    payload
+            )
+    }
 
     constructor(payload: Consumer<JsonArray>) : this(JsonArray(), null, Target.PROFILE) {
-        payload.accept(this.payload.asJsonArray)
+        var content = JsonArray()
+        payload.accept(content)
+
+        if (Json.isGzip(content))
+            content = Json.gzipDecompress(content).asJsonArray
+
+        this.payload.asJsonArray.addAll(content)
+    }
+
+    override fun serialize(): JsonObject {
+        val json = super.serialize()
+        json.add("content", Json.gzipCompress(this.payload))
+
+        return json
     }
 }
 
