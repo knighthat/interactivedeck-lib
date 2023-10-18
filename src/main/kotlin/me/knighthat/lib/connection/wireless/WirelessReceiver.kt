02694ab17b343d901612963df9372232d3fb4c10
KnightHat <knightchanelgaming@gmail.com>
2023-10-18 17:33:34 -0500

    introduced system to send and receive messages over Socket

---
+++ src/main/kotlin/me/knighthat/lib/connection/wireless/WirelessReceiver.kt
@@ -0,0 +1,53 @@
+package me.knighthat.lib.connection.wireless
+
+import com.google.gson.JsonParseException
+import com.google.gson.JsonParser
+import me.knighthat.lib.connection.request.AbstractRequestHandler
+import me.knighthat.lib.connection.request.Request
+import me.knighthat.lib.logging.Log
+import java.io.InputStream
+
+class WirelessReceiver(
+    private val inStream: InputStream,
+    private val buffer: ByteArray,
+    private val handler: AbstractRequestHandler
+) : Runnable {
+
+
+    private fun process(payload: String) {
+        Log.deb("Processing: $payload")
+
+        try {
+            val json = JsonParser.parseString(payload).asJsonObject
+            handler.process(Request.fromJson(json))
+        } catch (e: JsonParseException) {
+            Log.exc("json parse failed!", e, false)
+        }
+    }
+
+    override fun run() {
+        var bytesRead: Int
+        var finalStr = ""
+
+        while (inStream.read(buffer).also { bytesRead = it } != -1) {
+            val decoded = String(buffer, 0, bytesRead)
+            Log.deb("Received: $decoded")
+
+            val sliced = decoded.split('\u0000')
+
+            finalStr += sliced[0]
+            if (sliced.size > 1)
+                process(finalStr)
+
+            for (index in 1 ..< sliced.size) {
+
+                if (sliced.size - 1 == index) {
+                    finalStr = sliced[index]
+                    break
+                }
+
+                process(sliced[index])
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/me/knighthat/lib/connection/wireless/WirelessSender.kt
@@ -0,0 +1,62 @@
+package me.knighthat.lib.connection.wireless
+
+import me.knighthat.lib.connection.Connection
+import me.knighthat.lib.connection.request.Request
+import me.knighthat.lib.logging.Log
+import java.io.IOException
+import java.io.OutputStream
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.TimeUnit
+
+class WirelessSender(private val outStream: OutputStream) : Thread() {
+
+    companion object {
+
+        private val QUEUE = LinkedBlockingQueue<Request>(20)
+
+        fun send(request: Request) {
+            try {
+                if (Connection.isConnected())
+                    QUEUE.offer(request, 5000L, TimeUnit.MILLISECONDS)
+                else {
+                    Log.warn("Failed to send request. Connection is not established!")
+                    Log.reportBug()
+                    Log.deb("Request: $request")
+                }
+            } catch (e: InterruptedException) {
+                Log.exc("Thread was interrupted while send request is pending!", e, true)
+            }
+        }
+    }
+
+    init {
+        name = "NET/O"
+        QUEUE.clear()
+    }
+
+    override fun run() {
+        while (!interrupted()) {
+            try {
+                val serialized = QUEUE.take().toString()
+                Log.deb("Sending: $serialized")
+
+                outStream.write(appendNull(serialized))
+                outStream.flush()
+            } catch (e: InterruptedException) {
+
+                //TODO Needs proper error handling
+                if (!Connection.isConnected())
+                    return
+
+                Log.exc("Thread interrupted!", e, true)
+                break
+            } catch (e: IOException) {
+                //TODO Needs proper error handling
+                Log.exc("Error occurs while sending out request", e, true)
+                break
+            }
+        }
+    }
+
+    private fun appendNull(input: String): ByteArray = input.plus("\u0000").toByteArray()
+}
