a17c3b7ce0b6a31aeb14c0ce683281ef98cd4bf9
KnightHat <knightchanelgaming@gmail.com>
2023-10-18 10:06:39 -0500

    added abstract class to handle Requests

---
+++ src/main/kotlin/me/knighthat/lib/connection/request/AbstractRequestHandler.kt
@@ -0,0 +1,30 @@
+package me.knighthat.lib.connection.request
+
+import me.knighthat.lib.connection.Connection
+
+abstract class AbstractRequestHandler {
+
+    protected abstract fun handleAddRequest(request: AddRequest)
+
+    protected abstract fun handleRemoveRequest(request: RemoveRequest)
+
+    protected abstract fun handleUpdateRequest(request: UpdateRequest)
+
+    protected abstract fun handlePairRequest(request: Request)
+
+    protected abstract fun handleActionRequest(request: Request)
+
+    fun process(request: Request) {
+        if (request is RequiredConnection && !Connection.isConnected())
+            return
+
+        when (request.type) {
+
+            Request.RequestType.ADD    -> handleAddRequest(request as AddRequest)
+            Request.RequestType.REMOVE -> handleRemoveRequest(request as RemoveRequest)
+            Request.RequestType.UPDATE -> handleUpdateRequest(request as UpdateRequest)
+            Request.RequestType.PAIR   -> handlePairRequest(request)
+            Request.RequestType.ACTION -> handleActionRequest(request)
+        }
+    }
+}
