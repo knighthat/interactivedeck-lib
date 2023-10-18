b0a6edbf871679a49671e86a29c2468cae9fe933
KnightHat <knightchanelgaming@gmail.com>
2023-10-18 17:30:57 -0500

    parse action from json

--- src/main/kotlin/me/knighthat/lib/connection/action/Action.kt
+++ src/main/kotlin/me/knighthat/lib/connection/action/Action.kt
@@ -2,6 +2,7 @@ package me.knighthat.lib.connection.action
 
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import me.knighthat.lib.exception.ActionException
 import me.knighthat.lib.json.JsonSerializable
 import java.util.*
 
@@ -9,6 +10,26 @@ class Action(
     val uuid: UUID,
     val type: ActionType
 ) : JsonSerializable {
+
+    companion object {
+        @JvmStatic
+        fun fromJson(element: JsonElement): Action {
+            if (!element.isJsonObject)
+                throw IllegalStateException("invalid format!")
+
+            val json = element.asJsonObject
+
+            if (!json.has("uuid"))
+                throw ActionException("missing uuid!")
+            if (!json.has("type"))
+                throw ActionException("missing type!")
+
+            val uuid = UUID.fromString(json["uuid"].asString)
+            val type = ActionType.valueOf(json["type"].asString)
+
+            return Action(uuid, type)
+        }
+    }
 
     override fun serialize(): JsonElement {
         val json = JsonObject()
