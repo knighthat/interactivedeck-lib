52547053bc04e4fa6a2965241627180340030209
KnightHat <knightchanelgaming@gmail.com>
2023-10-22 20:50:46 -0500

    shortens UUID to its last five digits

--- src/main/kotlin/me/knighthat/lib/util/ShortUUID.java
+++ src/main/kotlin/me/knighthat/lib/util/ShortUUID.java
@@ -1,6 +1,5 @@
 package me.knighthat.lib.util;
 
-import me.knighthat.lib.logging.Log;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
@@ -9,10 +8,6 @@ public class ShortUUID {
 
     public static @NotNull String from( @NotNull UUID uuid ) {
         String uuidStr = uuid.toString();
-        String shortUuid = uuidStr.substring( uuidStr.length() - 5 );
-
-        Log.deb( "Converted " + uuid + " to " + shortUuid );
-
-        return shortUuid;
+        return uuidStr.substring( uuidStr.length() - 5 );
     }
 }
