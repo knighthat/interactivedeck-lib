5d74195025ad653c69e6af781b208471a55c3eaf
KnightHat <knightchanelgaming@gmail.com>
2023-11-01 21:07:57 -0500

    updated test

--- src/test/kotlin/me/knighthat/lib/connection/request/RequestTest.kt
+++ src/test/kotlin/me/knighthat/lib/connection/request/RequestTest.kt
@@ -76,10 +76,14 @@ class RequestTest {
     @Test
     fun requestExceptions() {
 
-        val fileNames = arrayOf("MissingContent_PairRequest", "MissingTarget_UpdateRequest", "MissingType", "MissingUUID_RemoveRequest")
+        val fileNames = arrayOf("MissingContent_PairRequest", "MissingTarget_UpdateRequest", "MissingType", "MissingUUID_UpdateRequest")
         for (name in fileNames) {
             val json = get(name)
-            Assertions.assertThrows(RequestException::class.java) { Request.fromJson(json!!) }
+            if (json == null) {
+                Assertions.fail<String> { "$name returns null!" }
+                continue
+            }
+            Assertions.assertThrows(RequestException::class.java) { Request.fromJson(json) }
         }
 
     }
@@ -108,61 +112,4 @@ class RequestTest {
         val json = get("WrongPayloadFormat_AddRequest") ?: return
         Assertions.assertThrows(IllegalStateException::class.java) { Request.fromJson(json) }
     }
-
-    //@Test
-    //fun perfectRequest() {
-    //    val perfectRequest = JsonParser.parseString("{\"type\":\"PAIR\",\"content\":{\"task\":null}}")
-    //
-    //    var perfReq: Request? = null
-    //    Assertions.assertDoesNotThrow { perfReq = Request.fromJson(perfectRequest) }
-    //    Assertions.assertNotNull(perfReq)
-    //    Assertions.assertEquals(perfReq!!.type, Request.RequestType.PAIR)
-    //    Assertions.assertInstanceOf(JsonObject::class.java, perfReq!!.payload)
-    //}
-    //
-    //@Test
-    //fun perfectButtonRequest() {
-    //    val perfectButtonRequest =
-    //        JsonParser.parseString("{\"type\":\"ADD\",\"content\":[\"72884bd1-7852-43c3-b877-a3d051f1253d\",\"1dc48e36-3f5d-4141-85ae-7d4dfec878fd\"],\"target\":\"BUTTON\",\"uuid\":\"78252640-6a68-4981-84be-a66340defb76\"} ")
-    //
-    //    var perfBtnReq: Request? = null
-    //    Assertions.assertDoesNotThrow { perfBtnReq = Request.fromJson(perfectButtonRequest) }
-    //    Assertions.assertNotNull(perfBtnReq)
-    //    Assertions.assertInstanceOf(AddRequest::class.java, perfBtnReq)
-    //    Assertions.assertEquals((perfBtnReq!! as TargetedRequest).target, TargetedRequest.Target.BUTTON)
-    //    Assertions.assertNotNull((perfBtnReq!! as TargetedRequest).uuid)
-    //    Assertions.assertFalse(Json.isGzip(perfBtnReq!!.payload))
-    //
-    //    val payload = perfBtnReq!!.serialize().get("content")
-    //    Assertions.assertTrue(Json.isGzip(payload))
-    //}
-    //
-    //@Test
-    //fun perfectProfileRequest() {
-    //    val perfectProfileRequest =
-    //        JsonParser.parseString("{\"type\":\"REMOVE\",\"content\":[\"72884bd1-7852-43c3-b877-a3d051f1253d\",\"1dc48e36-3f5d-4141-85ae-7d4dfec878fd\"],\"target\":\"PROFILE\",\"uuid\":null}")
-    //
-    //    var perfProfReq: Request? = null
-    //    Assertions.assertDoesNotThrow { perfProfReq = Request.fromJson(perfectProfileRequest) }
-    //    Assertions.assertNotNull(perfProfReq)
-    //    Assertions.assertInstanceOf(TargetedRequest::class.java, perfProfReq)
-    //    Assertions.assertEquals((perfProfReq!! as TargetedRequest).target, TargetedRequest.Target.PROFILE)
-    //    Assertions.assertNull((perfProfReq!! as TargetedRequest).uuid)
-    //
-    //    val payload = perfProfReq!!.serialize().get("content")
-    //    Assertions.assertFalse(Json.isGzip(payload))
-    //}
-    //
-    //@Test
-    //fun imperfectRequest() {
-    //    val wrongType = JsonParser.parseString("[\"ADD\",null]")
-    //    val missingType = JsonParser.parseString("{\"content\":{\"task\":null}}")
-    //    val missingContent = JsonParser.parseString("{\"type\":\"ADD\"}")
-    //    val missingTarget = JsonParser.parseString("{\"type\":\"ADD\",\"content\":{\"task\":null},\"uuid\":null}")
-    //
-    //    Assertions.assertThrows(RequestException::class.java) { Request.fromJson(wrongType) }
-    //    Assertions.assertThrows(RequestException::class.java) { Request.fromJson(missingContent) }
-    //    Assertions.assertThrows(RequestException::class.java) { Request.fromJson(missingType) }
-    //    Assertions.assertThrows(RequestException::class.java) { Request.fromJson(missingTarget) }
-    //}
 }
